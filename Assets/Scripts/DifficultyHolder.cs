d369dc67a3cc2fb63e1ef8cc70b7d0f3040f7495
louie <heloisam.silva@hotmail.com>
2023-12-07 19:38:21 -0300

    difficulty selection and main character simple animation

---
+++ Assets/Scripts/DifficultyHolder.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DifficultyHolder : MonoBehaviour
+{
+    public static DifficultyHolder instance;
+
+    //Diffulcty index: 1 - Easy / 2 - Normal / 3 - Hard
+    [SerializeField] private int difficultyChosen = 2;
+
+    private void Awake()
+    {
+        //creates the instance of the object that will hold the difficulty chosen information, keeping it from being destroyed
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void SetDifficulty(int difficulty) { difficultyChosen = difficulty; }
+    public int GetDifficultyChosen() { return difficultyChosen; }
+}
