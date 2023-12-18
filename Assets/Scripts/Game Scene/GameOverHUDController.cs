29cb3ff41467d0571f366fe150efc30992086472
louie <heloisam.silva@hotmail.com>
2023-12-18 16:05:21 -0300

    using serialized fields instead of .Find method as suggested

--- Assets/Scripts/Game Scene/GameOverHUDController.cs
+++ Assets/Scripts/Game Scene/GameOverHUDController.cs
@@ -3,23 +3,21 @@ using UnityEngine;
 
 public class GameOverHUDController : MonoBehaviour
 {  
-    private Transform gameOverPanel;
-    private Transform highscoreMessage;
+    [SerializeField] private Transform gameOverPanel;
+    [SerializeField] private Transform highscoreMessage;
 
     private void Start()
     {
-        gameOverPanel = gameObject.transform.Find("GameOverPanel");
-        highscoreMessage = gameOverPanel.transform.Find("HighscoreMessage");
         highscoreMessage?.gameObject.SetActive(false);
     }
 
     public void OnCharacterDeath()
     {
-        gameOverPanel.gameObject.SetActive(true);
+        gameOverPanel?.gameObject.SetActive(true);
     }
 
     public void OnNewHighscoreReached()
     {
-        highscoreMessage.gameObject.SetActive(true);
+        highscoreMessage?.gameObject.SetActive(true);
     }
 }
